e0ad9255b84092b9afc27f4dae8f9d18f8076abc
Fabian Dasler <famabiker@gmail.com>
2022-05-06 14:39:48 +0200

    add server

---
+++ KrakenServer/Server.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Text.Encodings;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace KrakenServer
+{
+	public class Server : IAsyncDisposable
+	{
+		/// <summary>
+        /// The ip-adress of the host
+        /// </summary>
+		public string HostIp { get; private set; }
+
+		/// <summary>
+        /// the port on which the server is running on
+        /// </summary>
+		public int Port { get; private set; }
+
+		/// <summary>
+        /// the <see cref="TcpClient"/> which handles the connection
+        /// </summary>
+		TcpClient? _client { get; set; }
+
+		/// <summary>
+        /// the <see cref="NetworkStream"/> from the <see cref="_client"/>
+        /// </summary>
+		NetworkStream? _stream { get; set; }
+
+		/// <summary>
+        /// The <see cref="TcpListener"/> to listen for incoming connections
+        /// </summary>
+		TcpListener _listener { get; }
+
+		/// <summary>
+        /// the data that arraives via the network stream
+        /// </summary>
+		public string Data { get; set; }
+
+		/// <summary>
+        /// Indicates the Status of the Server(-operations) with a <see cref="bool"/>
+        /// <see langword="false"/> indicates that an error occured
+        /// </summary>
+		public bool Status { get; private set; }
+
+		/// <summary>
+		/// Creates an instanz of the <see cref="Server"/> class
+		/// </summary>
+		/// <param name="host">The ip on which the <see cref="TcpListener"/> runs on</param>
+		/// <param name="port">The port on which the <see cref="TcpListener"/> runs on</param>
+		public Server(string host, int port)
+		{
+			HostIp = host;
+			Port = port;
+			Data = string.Empty;
+
+			_listener = new TcpListener(IPAddress.Parse(HostIp), Port);
+		}
+
+
+		public async Task Listen()
+        {
+			_listener.Start();
+			Console.WriteLine($"Listener is setup on port {Port}");
+			_client = await _listener.AcceptTcpClientAsync();
+
+            if (_client.Connected)
+            {
+				_stream = _client.GetStream();
+                Console.WriteLine($"TcpClient {((IPEndPoint)_client.Client.RemoteEndPoint!).Address} connects to the Server");
+				Status = await Receive();
+            }
+        }
+
+		public async Task<bool> Receive()
+        {
+			byte[] tempBuffer = Array.Empty<byte>();
+
+			try
+			{
+				Console.WriteLine($"receiveing from {((IPEndPoint)_client!.Client.RemoteEndPoint!).Address}:");
+
+				if (!_stream!.CanRead)
+					return false;
+
+				int received = await _stream.ReadAsync(tempBuffer);
+				await _stream.FlushAsync();
+
+				if (received > 0)
+				{
+					Data = Encoding.UTF8.GetString(tempBuffer, 0, tempBuffer.Length);
+					Console.WriteLine(Data);
+					return true;
+				}
+				else
+					return false;
+			}
+			catch(Exception e)
+            {
+				Data = "";
+                Console.WriteLine("An error occured on receiving: " + e.Message);
+				return false;
+            }
+        }
+
+		public async Task<bool> Send(string msg)
+        {
+            try
+            {
+				byte[] temBuffer = Encoding.UTF8.GetBytes(msg);
+				await _stream!.WriteAsync(temBuffer);
+
+				return true;
+            }
+			catch(Exception e)
+            {
+                Console.WriteLine("An error occured on sending: " + e.Message);
+				return false;
+            }
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+			_stream!.Close();
+			await _stream!.DisposeAsync();
+			_client!.Close();
+			_client!.Dispose();
+			_listener.Stop();
+			Status = false;
+			Data = HostIp = "";
+			Port = 0;
+        }
+    }
+}
+
